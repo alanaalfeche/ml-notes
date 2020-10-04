f92d49e244ae4e1df4ce23d5ce79899911779477
alanaalfeche <alanaalfeche@gmail.com>
2020-10-04 11:09:35 -0500

    Add scaling and normalization notes

---
+++ kaggle/data_cleaning/scaling_and_normalization.py
@@ -0,0 +1,73 @@
+# # Get our environment set up 
+# To practice scaling and normalization, we're going to use a [dataset of Kickstarter campaigns](https://www.kaggle.com/kemical/kickstarter-projects). 
+import pandas as pd
+import numpy as np
+
+# for Box-Cox Transformation
+from scipy import stats
+
+# for min_max scaling
+from mlxtend.preprocessing import minmax_scaling
+
+# plotting modules
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# read in all our data
+kickstarters_2017 = pd.read_csv("../input/kickstarter-projects/ks-projects-201801.csv")
+np.random.seed(0)
+
+# # 1) Scaling - Change the range of data so that it fits within a specific scale e.g. 0-100, 0-1
+# Let's start by scaling the goals of each campaign, which is how much money they were asking for. 
+
+# select the usd_goal_real column
+original_data = pd.DataFrame(kickstarters_2017.usd_goal_real)
+
+# scale the goals from 0 to 1
+scaled_data = minmax_scaling(original_data, columns=['usd_goal_real'])
+
+# plot the original & scaled data together to compare
+fig, ax=plt.subplots(1,2,figsize=(15,3))
+sns.distplot(kickstarters_2017.usd_goal_real, ax=ax[0])
+ax[0].set_title("Original Data")
+sns.distplot(scaled_data, ax=ax[1])
+ax[1].set_title("Scaled data")
+
+# After scaling, all values lie between 0 and 1 (you can read this in the horizontal axis of the second plot above, and we verify in the code cell below).
+print('Original data\nPreview:\n', original_data.head())
+print('Minimum value:', float(original_data.min()),
+      '\nMaximum value:', float(original_data.max()))
+print('_'*30)
+
+print('\nScaled data\nPreview:\n', scaled_data.head())
+print('Minimum value:', float(scaled_data.min()),
+      '\nMaximum value:', float(scaled_data.max()))
+
+# # 2) Normalization - Change the shape of the distribution of your data to be more like of a normal 'gaussian' distribution 
+# Now you'll practice normalization. We begin by normalizing the amount of money pledged to each campaign.
+
+# get the index of all positive pledges (Box-Cox only takes positive values)
+index_of_positive_pledges = kickstarters_2017.usd_pledged_real > 0
+
+# get only positive pledges (using their indexes)
+positive_pledges = kickstarters_2017.usd_pledged_real.loc[index_of_positive_pledges]
+
+# normalize the pledges (w/ Box-Cox)
+normalized_pledges = pd.Series(stats.boxcox(positive_pledges)[0], 
+                               name='usd_pledged_real', index=positive_pledges.index)
+
+# plot both together to compare
+fig, ax=plt.subplots(1,2,figsize=(15,3))
+sns.distplot(positive_pledges, ax=ax[0])
+ax[0].set_title("Original Data")
+sns.distplot(normalized_pledges, ax=ax[1])
+ax[1].set_title("Normalized data")
+
+print('Original data\nPreview:\n', positive_pledges.head())
+print('Minimum value:', float(positive_pledges.min()),
+      '\nMaximum value:', float(positive_pledges.max()))
+print('_'*30)
+
+print('\nNormalized data\nPreview:\n', normalized_pledges.head())
+print('Minimum value:', float(normalized_pledges.min()),
+      '\nMaximum value:', float(normalized_pledges.max()))
